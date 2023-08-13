9906857193e7c1cdab56926e014218de02dbcdbb
Ivan Kolesnikov <ivan@Ivans-MacBook-Pro.local>
2023-08-13 20:48:40 +0300

    Create url record

---
+++ internal/lib/string/str.go
@@ -0,0 +1,19 @@
+package str
+
+import (
+	"math/rand"
+	"time"
+)
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func Generate(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -3,6 +3,7 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
+	str "url-shortener/internal/lib/string"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -21,7 +22,7 @@ func New(storagePath string) (*Storage, error) {
 
 	stmt, err := db.Prepare(`
 		CREATE TABLE IF NOT EXISTS url (
-		    id INT PRIMARY KEY,
+		    id INTEGER PRIMARY KEY,
 		    alias VARCHAR (10) NOT NULL UNIQUE,
 		    url TEXT NOT NULL
 		);
@@ -39,4 +40,31 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) Create(urlValue string) (int64, error) {
+	const op = "storage.sqlite.Create"
+
+	stmt, err := s.db.Prepare("INSERT INTO url (alias, url) VALUES (?, ?)")
+
+	if err != nil {
+		return 0, fmt.Errorf("%s: prepare statement: %w", op, err)
+	}
+
+	// TODO Check that alias is not exists in db
+	alias := str.Generate(6)
+
+	res, err := stmt.Exec(alias, urlValue)
+
+	if err != nil {
+		return 0, fmt.Errorf("%s: exec statement: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return 0, fmt.Errorf("%s: can't get last insert id: %w", op, err)
+	}
+
+	return id, nil
 }
